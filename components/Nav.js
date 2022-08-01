d5e339c7ca847b1ced19c8068cd1778c7a36f897
hardeylim <hardeylim@gmail.com>
2022-08-01 23:21:59 +0800

    Adds components and segregates styling

---
+++ components/Nav.js
@@ -0,0 +1,21 @@
+import styles from 'styles/Nav.module.css'
+import Link from "next/link";
+
+const Nav = () => {
+
+    return (
+
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <Link
+                    ></Link>
+                </li>
+            </ul>
+        </nav>
+
+    )
+}
+
+
+export default Nav
