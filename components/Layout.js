97daa6da6ae0fd4ec25be50ffe5011356527d39c
hardeylim <hardeylim@gmail.com>
2022-08-02 00:12:08 +0800

    Adds header

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import styles from '../styles/Layout.module.css'
 import Nav from "./Nav";
+import Header from "./Header";
 
 const Layout = ({children}) => {
     return (
         <>
             <Nav></Nav>
+            <Header>
+
+            </Header>
             <div className={styles.container}>
                 <main className={styles.main}>
                     {children}
